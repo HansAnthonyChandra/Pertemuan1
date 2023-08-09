0012eae1839c2a98a1713377f95726953a38373d
Hans_Anthony_Chandra <hans.anthony2605@gmail.com>
2023-08-09 14:39:15 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---Data Mahasiswa---");
+        System.out.println("Nama        : Hans Anthony Chandra");
+        System.out.println("NRP         : c14230095");
+        System.out.println("Mata kuliah : Algoritma dan Pemrograman");
+        System.out.println("Kelas       : B");
+        System.out.println("Tahun       : 2023/2024");
+        System.out.println("Semester    : Gasal");
+        System.out.println("------------------------");
+gitgi
+    }
+}
